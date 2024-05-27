c2acee0bcc4bdd68485bcad6025b39c549ea599c
Laurent Chardon <laurent.chardon@gmail.com>
2024-05-27 13:16:01 -0400

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='bsdnews',
+    version='1.0',
+    description='BSD News Reader Script',
+    author='Laurent Chardon',
+    author_email='laurent.chardon@gmail.com',
+    url='https://github.com/LaurentChardon/bsdnews',
+    license='BSD 3-Clause License',
+    py_modules=['bsdnews'],
+    install_requires=[
+        'requests',
+        'feedparser',
+        'beautifulsoup4',
+        'blessed',
+    ],
+    entry_points={
+        'console_scripts': [
+            'bsdnews=bsdnews:main',
+        ],
+    },
+)
